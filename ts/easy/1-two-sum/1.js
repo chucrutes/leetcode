25506fa998ceb53bc5b50f7d293da56db89c1f82
Thiago <tmelo387@gmail.com>
2025-03-23 10:39:29 -0300

    chore: adding compile command

--- ts/easy/1-two-sum/1.js
+++ ts/easy/1-two-sum/1.js
@@ -1,24 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.twoSum = twoSum;
 function removeUnusedValuesFromArray(arr, target) {
-  return arr.filter(function (item) {
-    return Math.abs(item) <= target;
-  });
+    return arr.filter(function (item) { return item <= target; });
 }
 function findSum(nums, target) {
-  var numbersAlreadySeen = new Map();
-  for (var index = 0; index < nums.length; index++) {
-    var element = nums[index];
-    var difference = target - element;
-    var isNumberAlreadySeen = numbersAlreadySeen.get(difference);
-    if (isNumberAlreadySeen === undefined) {
-      numbersAlreadySeen.set(element, index);
-      continue;
+    var numbersAlreadySeen = new Map();
+    for (var index = 0; index < nums.length; index++) {
+        var element = nums[index];
+        var difference = target - element;
+        var isNumberAlreadySeen = numbersAlreadySeen.get(difference);
+        if (isNumberAlreadySeen === undefined) {
+            numbersAlreadySeen.set(element, index);
+            continue;
+        }
+        return { isNumberAlreadySeen: isNumberAlreadySeen, index: index };
     }
-    return { isNumberAlreadySeen: isNumberAlreadySeen, index: index };
-  }
 }
 function twoSum(nums, target) {
-  var arr = removeUnusedValuesFromArray(nums, target);
-  var result = findSum(arr, target);
-  if (!result) return undefined;
-  return [result.index, result.isNumberAlreadySeen];
+    var arr = removeUnusedValuesFromArray(nums, target);
+    var result = findSum(arr, target);
+    if (!result)
+        return undefined;
+    return [result.index, result.isNumberAlreadySeen];
 }
