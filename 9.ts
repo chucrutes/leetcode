6923c71a092f61bdeee165e0d52f64f581731908
Thiago <tmelo387@gmail.com>
2025-04-01 11:49:25 -0300

    feat: exercise 9 completed

---
+++ 9.ts
@@ -0,0 +1,29 @@
+function reverse(x: number): number {
+  const isNegative = x < 0
+  let newNum = Math.abs(x)
+  let value = 0
+
+  while (newNum !== 0) {
+    const remaining = newNum % 10
+
+    newNum = Math.floor(newNum / 10)
+    value = value * 10 + remaining
+  }
+
+  const res = isNegative ? value + value * -2 : value
+  return res
+}
+
+const isPalindrome = (x: number): boolean => {
+  if (x < 0) {
+    return false
+  }
+
+  if (reverse(x) - x === 0) {
+    return true
+  }
+
+  return false
+}
+
+console.log(isPalindrome(120))
